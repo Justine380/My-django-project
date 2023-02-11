8d5608c8ec69201b157229cc8cc9196bb53c4e63
NabaalaJustine <justinenabaala@gmail.com>
2023-02-11 15:08:29 +0300

    user comment creation + participant and message deletion

--- knowledge/views.py
+++ knowledge/views.py
@@ -6,7 +6,7 @@ from django.db.models import Q
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login,logout
 from django.contrib.auth.forms import UserCreationForm
-from .models import Room, Topic
+from .models import Room, Topic, Message
 from .forms import RoomForm
 
 # # Create your views here.
@@ -79,8 +79,20 @@ def home(request):
 
 
 def room(request, pk):  
-    room = Room.objects.get(id=pk)     
-    context = {'room':room}
+    room = Room.objects.get(id=pk)
+    room_messages = room.message_set.all().order_by('-created')
+    participant = room.participants.all() 
+
+    if request.method == 'POST':
+        message = Message.objects.create(
+            user=request.user,
+            room=room,
+            body=request.POST.get('body')
+        )
+        room.participants.add(request.user)
+        return redirect('room', pk=room.id)
+
+    context = {'room':room, 'room_messages':room_messages,'participants':participant}
     return render(request, 'knowledge/room.html',context)
  
 
@@ -126,3 +138,17 @@ def deleteRoom(request,pk):
         room.delete()
         return redirect('home')   
     return render(request, 'knowledge/delete.html',{'obj':room})
+
+
+@login_required(login_url='login')
+def deleteMessage(request,pk):
+    message = Message.objects.get(id=pk)
+
+    if request.user != message.user:
+        return HttpResponse('You are not the owner to delete the room!!')
+
+    if request.method == 'POST':
+        message.delete()
+        return redirect('home')   
+    return render(request, 'knowledge/delete.html',{'obj':message})
+
